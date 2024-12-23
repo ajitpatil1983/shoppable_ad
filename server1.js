20603e6e217e0d9a40a79de878563352f1a53506
ajitpatil1983 <ajitpatil1983@gmail.com>
2024-12-23 19:30:30 +0530

    commit info

--- server1.js
+++ server1.js
@@ -12,7 +12,6 @@ console.log('Cache Path:', process.env.PUPPETEER_CACHE_DIR || 'Default path not 
 // Helper function to initialize Puppeteer
 async function initializeBrowser() {
   return await puppeteer.launch({
-    executablePath: '/tmp/puppeteer_cache/chrome/linux-131.0.6778.204/chrome-linux64/chrome',
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox'],
   });
